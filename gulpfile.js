1adda9e8481ce672f78d3a2e363408b037a57169
Marat <alembikovm@gmail.com>
2017-11-27 00:43:43 +0400

    Main page

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass        = require('gulp-sass');
+var concat      = require('gulp-concat'); // Подключаем gulp-concat (для конкатенации файлов)
+var uglify      = require('gulp-uglifyjs');
+var cssnano     = require('gulp-cssnano'); // Подключаем пакет для минификации CSS
+var rename      = require('gulp-rename');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
+    return gulp.src(['scss/bootstrap/scss/*.scss', 'src/scss/*.scss'])
         .pipe(sass())
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
@@ -12,20 +16,58 @@ gulp.task('sass', function() {
 
 // Move the javascript files into our /src/js folder
 gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
+    return gulp.src(['scss/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
         .pipe(gulp.dest("src/js"))
         .pipe(browserSync.stream());
 });
 
+gulp.task('scripts', function() {
+    return gulp.src([ // Берем все необходимые библиотеки
+        'src/libs/jquery/dist/jquery.min.js', // Берем jQuery
+        'src/libs/slick-carousel/slick/slick.min.js' // Берем Magnific Popup
+        ])
+        .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
+        .pipe(uglify()) // Сжимаем JS файл
+        .pipe(gulp.dest('src/js')); // Выгружаем в папку app/js
+});
+
+gulp.task('css-libs', ['sass'], function() {
+    return gulp.src('src/css/libs.css') // Выбираем файл для минификации
+        .pipe(cssnano()) // Сжимаем
+        .pipe(rename({suffix: '.min'})) // Добавляем суффикс .min
+        .pipe(gulp.dest('src/css')); // Выгружаем в папку app/css
+});
+
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
+gulp.task('serve', ['css-libs', 'scripts'], function() {
 
     browserSync.init({
         server: "./src"  
     });
 
-    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], ['sass']);
+    gulp.watch(['src/scss/*.scss'], ['sass']);
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['js','serve']);
+
+gulp.task('build', ['sass', 'scripts'], function() {
+    
+    var buildCss = gulp.src([ // Переносим CSS стили в продакшен
+        'src/css/*.css'
+        ])
+    .pipe(gulp.dest('dist/css'))
+
+    var buildFonts = gulp.src('src/fonts/**/*') // Переносим шрифты в продакшен
+    .pipe(gulp.dest('dist/fonts'))
+
+    var buildJs = gulp.src('src/js/**/*') // Переносим скрипты в продакшен
+    .pipe(gulp.dest('dist/js'))
+
+    var buildImg = gulp.src('src/images/**/*') // Переносим скрипты в продакшен
+    .pipe(gulp.dest('dist/images'))
+
+    var buildHtml = gulp.src('src/*.html') // Переносим HTML в продакшен
+    .pipe(gulp.dest('dist'));
+
+});
--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,54 @@ $(function () {
         e.preventDefault();
         $('.navigation').toggle('fast');
     })
+
+    $('.sl').slick({
+        appendArrows: $('.sl'),
+        prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><div class="arrow-wrap"><img src="images/icons/arrow-left.png" alt=""></div></button>',
+        nextArrow: '<button id="next" type="button" class="btn btn-juliet"><div class="arrow-wrap"><img src="images/icons/arrow-right.png" alt=""></div></button>',
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 767,
+                settings: {
+                    arrows: false,
+                    dots: false
+                }
+            }
+        ]
+    });
+
+    $('.sl-2').slick({
+        appendArrows: $('.second-slider'),
+        prevArrow: '<button id="prev-2" type="button" class="btn btn-juliet"><div class="arrow-wrap"><img src="images/icons/arrow-left.png" alt=""></div></button>',
+        nextArrow: '<button id="next-2" type="button" class="btn btn-juliet"><div class="arrow-wrap"><img src="images/icons/arrow-right.png" alt=""></div></button>',
+        dots: false,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true
+              }
+        },
+        {
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        }]
+    });
 })
